74aa69df78e97c3fbc202d5b12a4d69fd2f39c1c
Arslan Ovezov <arslan.datascience@gmail.com>
2025-06-29 20:07:53 +0500

    ars17

--- carwash_system_gui/main.py
+++ carwash_system_gui/main.py
@@ -5,3 +5,13 @@ import os
 from collections import defaultdict
 from openpyxl import Workbook
 import matplotlib.pyplot as plt
+
+LOG_FILE = "car_wash_log.txt"
+EXCEL_FILE = "car_wash_log.xlsx"
+
+PRICES = {
+    "Içi-daşy": 50,
+    "Gubka-aprat": 30,
+    "Daşy": 30,
+    "Içi": 20 
+}
