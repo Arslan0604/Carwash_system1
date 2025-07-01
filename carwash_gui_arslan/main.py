a5666975cf3adadd50bcabc0e140298990cd5841
Arslan Ovezov <arslan.datascience@gmail.com>
2025-07-01 20:49:20 +0500

    ars5

--- carwash_gui_arslan/main.py
+++ carwash_gui_arslan/main.py
@@ -17,3 +17,11 @@ PRICES = {
     "Daşy": 30,
     "Içi": 20 
 }
+# razobratsya s etoy funciey srochno
+def add_car_entry(plate_number, car_type, service_type):
+    now = datetime.datetime.now()
+    price = PRICES.get(service_type, 0)
+    entry = f"{now}, Plate: {plate_number}, Type: {car_type}, Service: {service_type}, Price: {price}\n"
+    with open(LOG_FILE, "a", encoding="utf-8") as file:
+        file.write(entry)
+    return price
