70170c48801d4d1b7e0b76c1f38ba403c28341b1
Arslan Ovezov <arslan.datascience@gmail.com>
2025-07-01 20:03:28 +0500

    ars5

---
+++ carwash_gui_arslan/main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox, scrolledtext
+import datetime
+import os
+from collections import defaultdict
+from openpyxl import Workbook
+import matplotlib.pyplot as plt
