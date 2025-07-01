6b631033ffec1e1f260ce12e7b8edeb63c76fdb1
Arslan Ovezov <arslan.datascience@gmail.com>
2025-07-01 20:38:20 +0500

    ars1

--- carwash_gui_arslan/main.py
+++ carwash_gui_arslan/main.py
@@ -9,3 +9,11 @@ import matplotlib.pyplot as plt
 
 LOG_FILE = "car_wash_log.txt"
 EXCEL_FILE = "car_wash_log.xlsx"
+
+
+PRICES = {
+    "I\u00e7i-da\u015fy": 50,
+    "Gubka-aprat": 30,
+    "Da\u015fy": 30,
+    "I\u00e7i": 20 
+}
