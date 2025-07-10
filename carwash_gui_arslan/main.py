3233d2ea1e0e3c26c292969754c244063399aed6
Arslan Ovezov <arslan.datascience@gmail.com>
2025-07-10 22:41:47 +0500

    ars1

--- carwash_gui_arslan/main.py
+++ carwash_gui_arslan/main.py
@@ -200,3 +200,6 @@ class CarWashApp:
         except ValueError:
             messagebox.showerror("Date Format Error", "Please use YYYY-MM-DD format.")
             return
+        
+        daily, _, _ = calculate_earnings_by_period(start_date, end_date)
+        plot_daily_earnings(daily)
