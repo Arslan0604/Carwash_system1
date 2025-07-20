e4f7fb0cc7425d56f3f59b5ac4e76373d83a7da7
Arslan Ovezov <arslan.datascience@gmail.com>
2025-07-20 21:54:02 +0500

    ars6

--- carwash1/main.py
+++ carwash1/main.py
@@ -1,3 +1,9 @@
 # Arslan you have to start from here your app
 # Sostavit structury srochno
 import tkinter as tk
+from tkinter import messagebox, scrolledtext
+import datetime
+import os
+from collections import defaultdict
+from openpyxl import Workbook
+import matplotlib.pyplot as plt
